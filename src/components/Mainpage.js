8a5ec7fd2bcf8285cc5a2b28956eb9c2ab01d56e
vyrozen <reyansah.zaki1@gmail.com>
2023-06-02 20:37:28 +0700

    modified:   package-lock.json 	modified:   package.json 	modified:   src/App.js 	modified:   src/components/Mainpage.js

--- src/components/Mainpage.js
+++ src/components/Mainpage.js
@@ -1,3 +1,4 @@
+import { Fade } from "react-awesome-reveal";
 import CharName from "./CharName";
 
 const Mainpage = () => {
@@ -29,64 +30,73 @@ const Mainpage = () => {
           ></img> */}
         </div>
         {/* <hr></hr> */}
-        <div className="px-6 pb-3 text-black">
-          <h1 className="font-semibold text-lg">Kenalin cewek gwe</h1>
-          <p className="text-slate-600 text-justify">
-            Namanya{" "}
-            <span className="font-semibold">Deswin Indria Cahya Ramadhani</span>
-            , biasa dipanggil{" "}
-            <span className="text-highlight">Deswin, Des, Win, Dedes, etc</span>{" "}
-            macem-macem pokoknya terserah temennya yang penting ngga yang
-            menjurus ke hal yang merendahkan. Kalo gue biasanya manggil{" "}
-            <span className="text-highlight">Ayang, Mbak Des, etc.</span> Deswin
-            lahir di Cilegon tanggal 11 November 2004. Nama bapaknya{" "}
-            <span className="font-semibold">Agus Cahyadi</span>, Deswin manggil
-            bapaknya dengan panggilan{" "}
-            <span className="text-highlight">Ayah.</span> Sedangkan ibunya
-            bernama <span className="text-highlight">Ade Norma,</span> Deswin
-            manggil ibunya pake sebutan{" "}
-            <span className="text-highlight">Mama.</span>
-            <br></br> Deswin sekolah SD di{" "}
-            <a href="https://insantama.sch.id/" className="href-link">
-              SIT Insantama Cilegon
-            </a>{" "}
-            terus lulus tahun 2016, dan lanjut ke jenjang SMP di{" "}
-            <a
-              href="https://id.wikipedia.org/wiki/SMP_Negeri_1_Cilegon"
-              className="href-link"
-            >
-              SMPN 1 Cilegon
-            </a>{" "}
-            dan lulus tahun 2020, Deswin abis itu lanjut jenjang pendidikannya
-            di{" "}
-            <a href="https://smansakotaserang.sch.id/" className="href-link">
-              SMAN 1 Kota Serang
-            </a>
-            , dan baru lulus pada tahun 2023 ini. Angkatan kita dikenal sebagai
-            angkatan COVID karena kami sekolah di era pandemi dari tahun 2019
-            sampe akhir tahun 2021. Deswin abis itu mau lanjutin karirnya jadi
-            dokter gigi, jadi dia harus kuliah kedokteran gigi dulu semoga saja
-            gwe sama Deswin tahun ini keterima di kampus pilihan kita
-            masing-masing, Amiin. Deswin juga punya hobi dari kecil Make Up gitu
-            wkwkwk, gwe kurang paham tapi keren karena dari hobi dia yang ini,
-            dia udah bisa ngehasilin duit sebagai{" "}
-            <a
-              href="https://id.wikipedia.org/wiki/Perias"
-              className="href-link italic font-mono"
-            >
-              MUA
-            </a>
-            . Serius keren banget cewek gwe, very proud of you Mbak Deswin!
-          </p>
-          {/* <hr className="my-10 border-t border-rose-200 shadow"></hr> */}
-        </div>
-        <div className="px-6 py-5">
-          <h1 className="font-semibold text-lg">Specialization</h1>
-          <div className="py-2 text-base">
-            Make Up
-            <div className="w-full h-2.5 bg-pink-400/30 rounded-full"></div>
+        <Fade>
+          <div className="px-6 pb-3 text-black">
+            <h1 className="font-semibold text-lg">Kenalin cewek gwe</h1>
+            <p className="text-slate-600 text-justify">
+              Namanya{" "}
+              <span className="font-semibold">
+                Deswin Indria Cahya Ramadhani
+              </span>
+              , biasa dipanggil{" "}
+              <span className="text-highlight">
+                Deswin, Des, Win, Dedes, etc
+              </span>{" "}
+              macem-macem pokoknya terserah temennya yang penting ngga yang
+              menjurus ke hal yang merendahkan. Kalo gue biasanya manggil{" "}
+              <span className="text-highlight">Ayang, Mbak Des, etc.</span>{" "}
+              Deswin lahir di Cilegon tanggal 11 November 2004. Nama bapaknya{" "}
+              <span className="font-semibold">Agus Cahyadi</span>, Deswin
+              manggil bapaknya dengan panggilan{" "}
+              <span className="text-highlight">Ayah.</span> Sedangkan ibunya
+              bernama <span className="text-highlight">Ade Norma,</span> Deswin
+              manggil ibunya pake sebutan{" "}
+              <span className="text-highlight">Mama.</span>
+              <br></br> Deswin sekolah SD di{" "}
+              <a href="https://insantama.sch.id/" className="href-link">
+                SIT Insantama Cilegon
+              </a>{" "}
+              terus lulus tahun 2016, dan lanjut ke jenjang SMP di{" "}
+              <a
+                href="https://id.wikipedia.org/wiki/SMP_Negeri_1_Cilegon"
+                className="href-link"
+              >
+                SMPN 1 Cilegon
+              </a>{" "}
+              dan lulus tahun 2020, Deswin abis itu lanjut jenjang pendidikannya
+              di{" "}
+              <a href="https://smansakotaserang.sch.id/" className="href-link">
+                SMAN 1 Kota Serang
+              </a>
+              , dan baru lulus pada tahun 2023 ini. Angkatan kita dikenal
+              sebagai angkatan COVID karena kami sekolah di era pandemi dari
+              tahun 2019 sampe akhir tahun 2021. Deswin abis itu mau lanjutin
+              karirnya jadi dokter gigi, jadi dia harus kuliah kedokteran gigi
+              dulu semoga saja gwe sama Deswin tahun ini keterima di kampus
+              pilihan kita masing-masing, Amiin. Deswin juga punya hobi dari
+              kecil Makeup gitu wkwkwk, gwe kurang paham tapi keren karena dari
+              hobi dia yang ini, dia udah bisa ngehasilin duit sebagai{" "}
+              <a
+                href="https://id.wikipedia.org/wiki/Perias"
+                className="href-link italic font-mono"
+              >
+                MUA
+              </a>
+              . Serius keren banget cewek gwe, very proud of you Mbak Deswin!
+            </p>
+            {/* <hr className="my-10 border-t border-rose-200 shadow"></hr> */}
           </div>
-        </div>
+        </Fade>
+
+        <Fade>
+          <div className="px-6 py-5">
+            <h1 className="font-semibold text-lg">Specialization</h1>
+            <div className="py-2 text-base">
+              Makeup
+              <div className="w-full h-2.5 bg-pink-400/30 rounded-full"></div>
+            </div>
+          </div>
+        </Fade>
       </div>
     </div>
   );
